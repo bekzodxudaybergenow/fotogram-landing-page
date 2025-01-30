9f412eaa63901902e7563836e96b164a05a08de3
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2025-01-30 23:48:47 +0500

    Update the javascript part of the header section

--- script/main.js
+++ script/main.js
@@ -1,17 +1,30 @@
 let header = document.querySelector('.header');
+let sidebar = document.querySelector('.sidebar');
+let headerMenu = document.querySelector('.header-menu');
+let sidebarClose = document.querySelector('.sidebar-close');
 
 
-let scrollManual = 0;
-window.addEventListener('scroll', () => {
 
-    if(scrollY > scrollManual) {
-        header.classList.add('headScroll');
-    }
-    else {
-        header.classList.remove('headScroll');
-    }
-    
-    scrollManual = scrollY;
+
+headerMenu.addEventListener('click', () => {
+    sidebar.classList.add('active');
 })
+sidebarClose.addEventListener('click', () => {
+    sidebar.classList.remove('active');
+})
+
+
+// let scrollManual = 0;
+// window.addEventListener('scroll', () => {
+
+//     if(scrollY > scrollManual) {
+//         header.classList.add('headScroll');
+//     }
+//     else {
+//         header.classList.remove('headScroll');
+//     }
+    
+//     scrollManual = scrollY;
+// })
 
 
